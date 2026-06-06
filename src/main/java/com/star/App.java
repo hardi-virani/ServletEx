234cce994a862252057e9a66d1d612652cbbd715
Hardi <hardivirani2003@gmail.com>
2026-06-06 10:10:10 -0400

    Created the servlet.

--- src/main/java/com/star/App.java
+++ src/main/java/com/star/App.java
@@ -1,13 +1,17 @@
 package com.star;
 
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws LifecycleException {
         System.out.println( "Hello World!" );
+        Tomcat tomcat = new Tomcat();
+        tomcat.start();
     }
 }
--- src/main/java/com/star/HelloServlet.java
+++ src/main/java/com/star/HelloServlet.java
@@ -1,4 +1,19 @@
 package com.star;
 
-public class HelloServlet {
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+
+    public void service(HttpServletRequest req, HttpServletResponse resp)  {
+
+        System.out.println("In Service");
+
+    }
+
+
+
 }
