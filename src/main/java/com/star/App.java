8145c0720e78e3b7246da544e81bb780d8e2ec0f
Hardi <hardivirani2003@gmail.com>
2026-06-06 10:45:40 -0400

    Updated my tomcat version.

--- src/main/java/com/star/App.java
+++ src/main/java/com/star/App.java
@@ -1,8 +1,10 @@
 package com.star;
 
+import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
 
+import jakarta.servlet.*;
 /**
  * Hello world!
  *
@@ -12,6 +14,15 @@ public class App
     public static void main( String[] args ) throws LifecycleException {
         System.out.println( "Hello World!" );
         Tomcat tomcat = new Tomcat();
+
+
+        Context context = tomcat.addContext("",null);
+        Tomcat.addServlet(context, "HelloServlet", new HelloServlet());
+        context.addServletMappingDecoded("/hello", "HelloServlet");
+
         tomcat.start();
+        tomcat.getServer().await();
+
     }
+
 }
--- src/main/java/com/star/HelloServlet.java
+++ src/main/java/com/star/HelloServlet.java
@@ -4,6 +4,8 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+
+
 import java.io.IOException;
 
 public class HelloServlet extends HttpServlet {
