14483337e9f3758c4d01415923a5dff1c0398373
Hardi <hardivirani2003@gmail.com>
2026-06-07 12:52:03 -0400

    Got data from the client.

--- src/main/java/com/star/App.java
+++ src/main/java/com/star/App.java
@@ -9,11 +9,14 @@ import jakarta.servlet.*;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args ) throws LifecycleException {
         System.out.println( "Hello World!" );
         Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+
+        tomcat.getConnector();
 
 
         Context context = tomcat.addContext("",null);
@@ -26,3 +29,51 @@ public class App
     }
 
 }
+
+
+//package com.star;
+//
+//import org.apache.catalina.Context;
+//import org.apache.catalina.LifecycleException;
+//import org.apache.catalina.startup.Tomcat;
+//
+//public class App {
+//
+//    public static void main(String[] args) throws LifecycleException {
+//
+//        System.out.println("Starting Embedded Tomcat...");
+//
+//        Tomcat tomcat = new Tomcat();
+//
+//        // Port on which Tomcat will listen
+//        tomcat.setPort(8080);
+//
+//
+//        tomcat.getConnector();
+//
+//        // Context path
+//        Context context =
+//                tomcat.addContext("", System.getProperty("java.io.tmpdir"));
+//
+//        // Register servlet
+//        Tomcat.addServlet(
+//                context,
+//                "HelloServlet",
+//                new HelloServlet()
+//        );
+//
+//        // URL Mapping
+//        context.addServletMappingDecoded(
+//                "/hello",
+//                "HelloServlet"
+//        );
+//
+//        tomcat.start();
+//
+//        System.out.println("Tomcat started successfully!");
+//        System.out.println("Open:");
+//        System.out.println("http://localhost:8080/hello");
+//
+//        tomcat.getServer().await();
+//    }
+//}
--- src/main/java/com/star/HelloServlet.java
+++ src/main/java/com/star/HelloServlet.java
@@ -14,8 +14,35 @@ public class HelloServlet extends HttpServlet {
 
         System.out.println("In Service");
 
+
     }
 
 
 
 }
+
+//package com.star;
+//
+//import jakarta.servlet.http.HttpServlet;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//
+//import java.io.IOException;
+//
+//public class HelloServlet extends HttpServlet {
+//
+//    @Override
+//    protected void service(
+//            HttpServletRequest req,
+//            HttpServletResponse resp)
+//            throws IOException {
+//
+//        System.out.println("In Service");
+//
+//        resp.setContentType("text/html");
+//
+//        resp.getWriter().println(
+//                "<h1>Hello from Embedded Tomcat Servlet!</h1>"
+//        );
+//    }
+//}
